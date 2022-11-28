111f5970ca2e15004c37c567b29b0dd20a9e33c3
Lautaro Darretche <lautarodarretche@hotmail.es>
2022-11-28 19:33:17 -0300

    (api/pdf) Deleted console.log()

--- src/api/pdf.js
+++ src/api/pdf.js
@@ -17,7 +17,6 @@ export class GeneratorPDF {
         }
 
         try {
-            console.log(data);
             let databaseController = new Database();
             let folderMonth;
             const formatMonth = { 1: 'enero', 2: 'febrero', 3: 'marzo', 4: 'abril', 5: 'mayo', 6: 'junio', 7: 'julio', 8: 'agosto', 9: 'septiembre', 10: 'octubre', 11: 'noviembre', 12: 'diciembre' };
