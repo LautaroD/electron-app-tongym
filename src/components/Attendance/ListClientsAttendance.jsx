0c97a7f263bf1b3968c9a3be773ca6d5f17c30a8
Lautaro Darretche <lautarodarretche@hotmail.es>
2022-11-30 18:34:45 -0300

    (components/ListClients) Added styles

--- src/components/Attendance/ListClientsAttendance.jsx
+++ src/components/Attendance/ListClientsAttendance.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Table, Icon } from 'semantic-ui-react';
-
+import './ListClientsAttendance.scss';
 
 export default function ListClientsAttendance({ list }) {
 
@@ -19,12 +19,12 @@ export default function ListClientsAttendance({ list }) {
                     {
                         list.map(element => {
                             return (element.payment === null || element.payment.length < 1)
-                                ? (<Table.Row error key={element.key} >
+                                ? (<Table.Row error key={element.key} className='tableError'>
                                     <Table.Cell width={6}>{element.text}</Table.Cell>
                                     <Table.Cell width={6}>{element.contacto}</Table.Cell>
                                     <Table.Cell width={6}><Icon name='attention' color='red' />Sin pago registrado</Table.Cell>
                                 </Table.Row>)
-                                : (<Table.Row positive key={element.key}>
+                                : (<Table.Row positive key={element.key} className='tableAfirmative'>
                                     <Table.Cell width={6}>{element.text}</Table.Cell>
                                     <Table.Cell width={6}>{element.contacto}</Table.Cell>
                                     <Table.Cell width={6}>{element.payment}</Table.Cell>
